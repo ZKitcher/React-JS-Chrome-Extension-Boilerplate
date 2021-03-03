fc5f3d0d99a7c5e97342dc2b7e665d00c5089b16
ZKitcher <49539587+ZKitcher@users.noreply.github.com>
2021-03-03 17:28:59 -0500

    Add files via upload

---
+++ contentBase/src/component/Options.js
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+import ListOptions from './ListOptions';
+
+function Options() {
+
+    return (
+        <div style={styles.content}>
+            <h1>Options</h1>
+        </div>
+    );
+}
+
+const styles = {
+    content: {
+        margin: '0',
+        fontFamily: 'font',
+        maxWidth: '700px',
+        margin: '10px auto',
+        padding: '10px',
+        fontFamily: "system-ui"
+    }
+}
+
+export default Options;
---
+++ contentBase/src/index-foreground.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { render } from 'react-dom';
+import Foreground from './component/Foreground.js';
+
+render(<Foreground />, document.querySelector('#foreground'))
